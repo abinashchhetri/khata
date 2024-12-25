365cf528d4eec92f91e1dad2c649cf64be88ccb7
abinashchhetri <abinashchhetri.work@gmail.com>
2024-12-24 22:34:32 -0600

    Add project files.

---
+++ khata/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace khata
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "khata" };
+        }
+    }
+}
---
+++ khata/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace khata
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
