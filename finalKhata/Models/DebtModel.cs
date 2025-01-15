a104624eb850424136931891772c0dba8cfe981b
abinashchhetri <abinashchhetri.work@gmail.com>
2025-01-14 22:54:38 -0600

    the debt adding and displaying feature done

--- finalKhata/Models/DebtModel.cs
+++ finalKhata/Models/DebtModel.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace finalKhata.Models
 {
-    internal class DebtModel
+    public class DebtModel
     {
+        public int id { get; set; }
+        public string Title { get; set; }
+        public int Amount { get; set; }
+        public string Note { get; set; } = string.Empty;
+
+        public string Source { get; set; }
+
+        public DateTime ReceivedDate { get; set; } = DateTime.Now;
+        public string SelectedTag { get; set; }
+
+        public DateTime ClearedDate { get; set; }
+
+
+        public bool isPaid { get; set; } = false;
+
+        public DebtModel(string title, int amount,  string source, string selectedTag, string note = "")
+        {
+
+            Title = title ?? throw new ArgumentNullException(nameof(title));
+            Amount = amount;
+            Note = note ;
+            Source = source ?? throw new ArgumentNullException(nameof(source));
+            SelectedTag = selectedTag ?? throw new ArgumentNullException(nameof(selectedTag));
+        }
     }
 }
---
+++ finalKhata/Services/DebtService.cs
@@ -0,0 +1,44 @@
+﻿using finalKhata.Configs;
+using finalKhata.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace finalKhata.Services
+{
+    public class DebtService
+    {
+
+        public static List<DebtModel> Debts { get; set; } = new List<DebtModel>();
+        public static String DebtsFilePath = FolderAndFiles.DebtsFilePath;
+
+        public static void AddDebt(DebtModel debt)
+        {
+            Debts.Add(debt);
+            UserService.User.TotalIncome +=  debt.Amount ;
+            UserService.User.TotalDebt += debt.Amount ;
+
+        }
+
+        public void GetSavedDebt()
+        {
+            string json = File.ReadAllText(DebtsFilePath);
+            Debts = JsonSerializer.Deserialize<List<DebtModel>>(json);
+
+        }
+
+        public void SaveAllDebt()
+        {
+            string json = JsonSerializer.Serialize(Debts);
+            File.WriteAllText(DebtsFilePath, json);
+
+        }
+
+        
+
+
+    }
+}
